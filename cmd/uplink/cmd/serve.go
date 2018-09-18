2808650b06ef3b87163e835f2361ca09d8573553
Natalie Ventura Villasana <nat@storj.io>
2018-09-18 15:22:40 -0400

    serve works when requesting url to content

--- cmd/uplink/cmd/serve.go
+++ cmd/uplink/cmd/serve.go
@@ -11,16 +11,14 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/gabriel-vasile/mimetype"
-	"github.com/julienschmidt/httprouter"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 
 	"storj.io/storj/pkg/paths"
-	"storj.io/storj/pkg/process"
 	"storj.io/storj/pkg/storage/buckets"
-	"storj.io/storj/pkg/utils"
 )
 
 var (
@@ -36,42 +34,24 @@ func init() {
 	addrFlag = serveCmd.Flags().String("addr", ":8080", "address to listen on")
 }
 
-type routes struct {
-	ctx  context.Context
-	args []string
-}
-
-func newRoutes(ctx context.Context, args []string) *routes {
-	return &routes{ctx: ctx, args: args}
-}
-
 func serve(cmd *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		return fmt.Errorf("No object specified to serve")
-	}
-	ctx := process.Ctx(cmd)
-	ro := newRoutes(ctx, args)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello here you are being served\n")
-	})
+	http.HandleFunc("/", serveGet)
 	fmt.Printf("Now listening on %s\n", *addrFlag)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s", *addrFlag), start(ro)))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s", *addrFlag), nil))
 
 	return nil
 }
 
-func start(ro *routes) *httprouter.Router {
-	router := httprouter.New()
-	router.GET("/", ro.serveGet)
-	// .POST later, which should add a new file (then website can reupload back into network)
-	// .PUT can check that thing exists first then do the post
-	// r.URL.Path is how to figure out what bucket/file (opaque string needs to be split)
-	return router
-}
+func serveGet(w http.ResponseWriter, r *http.Request) {
+	pathParts := strings.SplitN(r.URL.Path, "/", 3)
+	newURL := &url.URL{
+		Host:   pathParts[1],
+		Scheme: "sj",
+		Path:   pathParts[2],
+	}
 
-func (ro *routes) serveGet(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	rr, err := ro.handleTheGrab()
+	rr, err := handleTheGrab(newURL)
+	//first arg in url path needs to be set as host, everything else is the path
 	if err != nil {
 		http.Error(w, "bad request: err grabbing response", http.StatusBadRequest)
 		log.Fatal("err grabbing response", err)
@@ -98,19 +78,16 @@ func (ro *routes) serveGet(w http.ResponseWriter, r *http.Request, ps httprouter
 }
 
 // handleTheGrab gets ready to grab
-func (ro *routes) handleTheGrab() (io.ReadCloser, error) {
-	u0, err := utils.ParseURL(ro.args[0])
+func handleTheGrab(src *url.URL) (io.ReadCloser, error) {
+	ctx := context.Background()
+	bs, err := cfg.BucketStore(ctx)
 	if err != nil {
 		return nil, err
 	}
-	bs, err := cfg.BucketStore(ro.ctx)
-	if err != nil {
-		return nil, err
-	}
-	if u0.Host == "" {
+	if src.Host == "" {
 		return nil, fmt.Errorf("No bucket specified. Please use format sj://bucket/")
 	}
-	r, err := ro.grabObj(ro.ctx, bs, u0)
+	r, err := grabObj(ctx, bs, src)
 	if err != nil {
 		return nil, err
 	}
@@ -118,7 +95,7 @@ func (ro *routes) handleTheGrab() (io.ReadCloser, error) {
 }
 
 // grabObj returns s3 compatible object at args[0]
-func (ro *routes) grabObj(ctx context.Context, bs buckets.Store, srcObj *url.URL) (io.ReadCloser, error) {
+func grabObj(ctx context.Context, bs buckets.Store, srcObj *url.URL) (io.ReadCloser, error) {
 	if srcObj.Scheme == "" {
 		return nil, fmt.Errorf("Invalid source")
 	}
